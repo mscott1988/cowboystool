b655ac2dbed795daba1aa6c6e39f2a4029865b07
mscott1988 <michael@cowboysac.com>
2025-06-30 16:11:27 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('Estimator Loaded');
+let currentStep = 0;
+const steps = document.querySelectorAll('.step');
+
+function showStep(index) {
+  steps.forEach((step, i) => {
+    step.classList.toggle('active', i === index);
+  });
+  currentStep = index;
+}
+
+function nextStep() {
+  if (currentStep < steps.length - 1) {
+    showStep(currentStep + 1);
+  }
+}
+
+function prevStep() {
+  if (currentStep > 0) {
+    showStep(currentStep - 1);
+  }
+}
+
+document.getElementById('warrantyForm').addEventListener('submit', (e) => {
+  e.preventDefault();
+  alert('Warranty submitted!');
+});
